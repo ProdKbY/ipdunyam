720342c047ea934a3575b506a6ee7df4450e3635
ProdKbY <qing.qezer@gmail.com>
2026-01-15 16:51:37 +0300

    Add files via upload

---
+++ js/js/firebase.js
@@ -0,0 +1,16 @@
+// js/firebase.js  (GitHub Pages uyumlu)
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-auth.js";
+
+// 🔥 FIREBASE CONFIG
+const firebaseConfig = {
+  apiKey: "AIzaSyDN5LuEvNbPvfHxtOY713oP7x2V5wTB42I",
+  authDomain: "ipdunyam.firebaseapp.com",
+  projectId: "ipdunyam",
+  storageBucket: "ipdunyam.firebasestorage.app",
+  messagingSenderId: "824527303340",
+  appId: "1:824527303340:web:4d09f91d202eb69cbee509"
+};
+
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
